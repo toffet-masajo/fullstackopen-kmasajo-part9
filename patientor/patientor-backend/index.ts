0e2c4640c96439011a166f13bc9d3254e00887a5
Toffet Masajo <86598484+toffet-masajo@users.noreply.github.com>
2024-03-28 16:09:24 +0800

    9.9: Patientor backend, step2

--- patientor/patientor-backend/index.ts
+++ patientor/patientor-backend/index.ts
@@ -1,10 +1,13 @@
+import cors from "cors";
 import express from "express";
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
