8cb86823282b85c472ee879bd00a7f802318814f
Toffet Masajo <86598484+toffet-masajo@users.noreply.github.com>
2024-04-05 01:47:18 +0800

    Exercise 9.17

--- 9d-vite-typescript/part2-diary/src/App.tsx
+++ 9d-vite-typescript/part2-diary/src/App.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const URL = "http://localhost:3000";
+const URL = "http://localhost:3000/api/diaries";
 
 enum WeatherType {
   Sunny = "sunny",
@@ -19,26 +19,74 @@ enum VisibilityType {
 }
 
 interface DiaryEntry {
+  id: number;
   date: string;
   weather: WeatherType;
   visibility: VisibilityType;
 }
 
-interface ServerDiaryEntry extends DiaryEntry {
-  id: number;
-}
-
-function App() {
-  const [entries, setEntries] = useState<ServerDiaryEntry[]>([]);
+const App = () => {
+  const [entries, setEntries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState<string>("");
+  const [visibility, setVisibility] = useState<string>("");
+  const [weather, setWeather] = useState<string>("");
+  const [comment, setComment] = useState<string>("");
 
   useEffect(() => {
-    axios
-      .get<ServerDiaryEntry[]>(`${URL}/api/diaries`)
-      .then((response) => setEntries(response.data));
+    axios.get<DiaryEntry[]>(URL).then((response) => setEntries(response.data));
   });
+
+  const onAddNewDiaryEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const diaryEntryToAdd = {
+      date,
+      visibility,
+      weather,
+      comment,
+    };
+    axios
+      .post(URL, diaryEntryToAdd)
+      .then((response) => setEntries(entries.concat(response.data)));
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
 
   return (
     <>
+      <h2>Add new entry</h2>
+      <form onSubmit={onAddNewDiaryEntry}>
+        <div>
+          date:{" "}
+          <input
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility:{" "}
+          <input
+            value={visibility}
+            onChange={(event) => setVisibility(event.target.value)}
+          />
+        </div>
+        <div>
+          weather:{" "}
+          <input
+            value={weather}
+            onChange={(event) => setWeather(event.target.value)}
+          />
+        </div>
+        <div>
+          comment:{" "}
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button>add</button>
+      </form>
       <h2>Diary entries</h2>
       {entries.map((entry) => (
         <div key={entry.id}>
@@ -49,6 +97,6 @@ function App() {
       ))}
     </>
   );
-}
+};
 
 export default App;
