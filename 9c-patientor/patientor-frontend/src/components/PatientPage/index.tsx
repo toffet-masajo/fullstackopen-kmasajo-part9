dc3def00b8b8831485ebc612d0c7533923c941de
Toffet Masajo <86598484+toffet-masajo@users.noreply.github.com>
2024-04-19 01:46:10 +0800

    9.21: Patientor, step2

---
+++ 9c-patientor/patientor-frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,57 @@
+import { Typography, Box } from "@mui/material";
+import { Female, Male, QuestionMark } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+
+import { Gender, Patient } from "../../types";
+import patientService from "../../services/patients";
+
+interface PatientProps {
+  patientId: string;
+}
+
+interface GenderProps {
+  gender: Gender;
+}
+
+const GenderSymbol = ({ gender }: GenderProps) => {
+  if (gender === Gender.Male) return <Male fontSize="inherit" />;
+  if (gender === Gender.Female) return <Female fontSize="inherit" />;
+  if (gender === Gender.Other) return <QuestionMark fontSize="inherit" />;
+};
+
+const PatientPage = ({ patientId }: PatientProps) => {
+  const [patient, setPatient] = useState<Patient>();
+
+  useEffect(() => {
+    const fetchPatientData = async () => {
+      const patient = await patientService.getPatient(patientId);
+      setPatient(patient);
+    };
+    void fetchPatientData();
+  }, [patientId]);
+
+  return patient?.name ? (
+    <div>
+      <Box>
+        <Typography align="left" variant="h5">
+          {patient.name}
+          <GenderSymbol gender={patient.gender} />
+        </Typography>
+      </Box>
+      <Typography align="left" variant="body1">
+        ssn: {patient.ssn}
+      </Typography>
+      <Typography align="left" variant="body1">
+        occupation: {patient.occupation}
+      </Typography>
+    </div>
+  ) : (
+    <div>
+      <Typography variant="h5">
+        Patient does not exist in the system!
+      </Typography>
+    </div>
+  );
+};
+
+export default PatientPage;
--- 9c-patientor/patientor-frontend/src/types.ts
+++ 9c-patientor/patientor-frontend/src/types.ts
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -17,6 +17,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: [];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
